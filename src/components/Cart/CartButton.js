ea2c04780931c897e431d2554e30d6bfac0b413f
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-03-25 07:57:13 -0500

    Added automatic update to item amount in header display

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,29 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 import { cartActions } from '../../store/cartSlice';
+import { useEffect, useState } from 'react';
 
 
 const CartButton = (props) => {
-  const dispatch = useDispatch()
 
+  const dispatch = useDispatch()
   const handleCart = () => dispatch(cartActions.toggleCart())
+  const cartItems = useSelector (state => state.cart.items)
+  const [totalItems, setTotalItems] = useState (0)
+
+  useEffect (() => {
+    let totalAmount = 0
+    cartItems.forEach ((item) => {
+      totalAmount += item.amount
+    })
+    setTotalItems (totalAmount)
+
+  }, [cartItems])
 
   return (
     <button onClick={handleCart} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalItems}</span>
     </button>
   );
 };
